5a2f829cd3b49b684c619a4f54d23a0a7ef54245
temmaste <hothinh00191@gmail.com>
2023-10-24 15:19:29 +0700

    a

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
  
 app.listen(port,()=>{
-    console.log("connect to server port :"+port)
+    console.log("connect to server port :"+ port)
 })
-
+app.get('/',(req,res)=>res.json({message:"hello"}))
 const routeUser  = require('./router/userRoute')
 app.use(routeUser)
  
