df222603eda1167fd03253dec56e65fbbd7b8221
temmaste <hothinh00191@gmail.com>
2023-10-25 07:13:42 +0700

    first commit1

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,13 @@
 const { use } = require('../router/userRoute');
 const userService = require('../service/userService');
 const { trusted } = require('../util/db');
-const crypto = require('crypto');
+
 const userController = {
      loginUser :async (req,res)=>{
         try{
             const {user,password}  = req.body;
-        var userE = crypto.createHash('md5').update(user ).digest('hex');
-        var passwordE = crypto.createHash('md5').update(password ).digest('hex');
- 
-        if (await userService.loginUser(userE,passwordE) ){
+        
+        if (await userService.loginUser(user,password) ){
             res.status(200).json({message:"success"})
         }else{
             res.status(401).json({message:"no"})
